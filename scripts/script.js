19da6f92bb83c1d93361605410b826f83beb8d6c
jose cassio dos santos <88626989+JoseCassioSantos@users.noreply.github.com>
2022-01-26 08:34:21 -0300

    script

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,39 @@
 function gerar(){
 
+    let tnome = document.getElementById('nome')
+    let tmatricula = document.getElementById('matricula')
+    let temail = document.getElementById('email')
+
+    let formcont = {
+        nome:"",
+        matricula:"",
+        email:"",
+        ramal:"",
+        site:"",
+        andar:"",
+        op:"",
+        ferramenta:"",
+        maquinas1:"",
+        maquinas2:"",
+        descricao:"",
+        horario:"",
+        chamado:""
+
+    }
+
+
+formcont.nome = tnome.value
+formcont.matricula = tmatricula.value
+formcont.email = temail.value
+
+
+
+   
+    
     
 
-
+    console.log(formcont)
+    
 }
+
+
