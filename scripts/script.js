e536c86dac6902c56915d9f72f9869bf9c6698c6
jose cassio dos santos <88626989+JoseCassioSantos@users.noreply.github.com>
2022-01-26 21:09:08 -0300

    att cadastro!

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,16 @@ function gerar(){
     let tnome = document.getElementById('nome')
     let tmatricula = document.getElementById('matricula')
     let temail = document.getElementById('email')
+    let ttelefone = document.getElementById('telefone')
+    let tsite = document.getElementById('site')
+    let tandar = document.getElementById('andar')
+    let top = document.getElementById('op')
+    let tferramenta = document.getElementById('assunto')
+    let tqtd1 = document.getElementById('qtd1')
+    let tqtd2 = document.getElementById('qtd2')
+    let tdescricao = document.getElementById('descricao')
+    let tincio = document.getElementById('incio')
+    let tticket = document.getElementById('ticket')
 
     let formcont = {
         nome:"",
@@ -25,7 +35,16 @@ function gerar(){
 formcont.nome = tnome.value
 formcont.matricula = tmatricula.value
 formcont.email = temail.value
-
+formcont.ramal = ttelefone.value
+formcont.site = tsite.value
+formcont.andar = tandar.value
+formcont.op = top.value
+formcont.ferramenta = tferramenta.value
+formcont.maquinas1 = tqtd1.value
+formcont.maquinas2 = tqtd2.value
+formcont.descricao = tdescricao.value
+formcont.horario = tincio.value
+formcont.chamado = tticket.value
 
 
    
